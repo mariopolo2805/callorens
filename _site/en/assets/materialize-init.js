06f65175edc1f7d87e39a52f4a5f657769a87b72
Mario Antonio Polo Daza <marioapd@ext.inditex.com>
2019-09-14 22:09:23 +0200

    Developed embers section

--- _site/en/assets/materialize-init.js
+++ _site/en/assets/materialize-init.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".parallax").parallax(),$(".sidenav").sidenav(),$(".modal").modal({inDuration:300,outDuration:200}),$(".collapsible").collapsible()});
+$(document).ready(function(){function a(){$(".carousel").carousel("next"),setTimeout(a,5e3)}$(".parallax").parallax(),$(".sidenav").sidenav(),$(".modal").modal({inDuration:300,outDuration:200}),$(".collapsible").collapsible(),$(".carousel").carousel(),$(".carousel.carousel-slider").carousel({fullWidth:!0,indicators:!0}),a()});
--- js/materialize-init.js
+++ js/materialize-init.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
     outDuration: 200, // Transition out duration
   });
   $('.collapsible').collapsible();
+  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+  // Carousel
+  autoplay();
+  function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 5000);
+  }
 });
